68b40df69d397373f047175f7280bcf8f6ef9e80
casmir <chijiokeonyiba@yahoo.com>
2020-01-09 03:20:54 +0000

    inishing mobile hamburger

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react'
-import styled,{ThemeProvider as Theme } from 'styled-components';
+import styled from 'styled-components';
 
 const theme = {
     primary: 'teal',
--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import UnorderedList from './UnorderedList';
 
 
 const HamburgerTag = styled.div`
